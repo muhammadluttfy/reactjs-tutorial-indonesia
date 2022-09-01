5b937127c2ed4d5883904f9a1a1108ac2bad7b68
muhamammadluttfy <muhammadlutfi.storage@gmail.com>
2022-09-01 21:07:16 +0700

    initial commit | memahami konsep dasat StateLess Component dan StateFull Component (5)

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HelloComponent.css";
+
+const HelloComponent = () => {
+  return (
+    <p className="text-p">Hello Functional Component | Statelest component</p>
+  );
+};
+
+export default HelloComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HelloComponent from "./component/HelloComponent";
+import StateFullComponent from "./container/StateFullComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
+    <StateFullComponent />
   </React.StrictMode>
 );
 
